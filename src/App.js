1679107af5c93177d6d834d4ec67b211ed75187f
himanshush200599 <himanshush214@gmail.com>
2018-09-15 01:53:52 -0400

    set up components

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Post from './components/Post';
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Post app with Redux</h1>
+        </header>
+        <Post />
+      </div>
+    );
+  }
+}
+
+export default App;
